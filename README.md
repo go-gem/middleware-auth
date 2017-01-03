51f713240a0ace29bc1c62785c31e3f136db527a
razonyang@gmail.com <HeadwindFly@163.com>
2017-01-03 22:18:54 +0800

    Added example

--- README.md
+++ README.md
@@ -16,4 +16,70 @@ $ go get -u github.com/go-gem/middleware-auth
 
 **Example**
 
-Please refer to https://github.com/go-gem/examples/tree/master/auth.
+```
+package main
+
+import (
+	"github.com/abbot/go-http-auth"
+	"github.com/go-gem/gem"
+	"github.com/go-gem/middleware-auth"
+)
+
+var (
+	salt     = []byte("salt")
+	magic    = []byte("$1$")
+	username = "foo"
+	password = []byte("bar")
+)
+
+// basic auth middleware
+var (
+	basicPasswd = string(auth.MD5Crypt(password, salt, magic))
+
+	basicAuthenticator = auth.NewBasicAuthenticator("", func(user, realm string) string {
+		if user == username {
+			return basicPasswd
+		}
+		return ""
+	})
+
+	basicAuth = authmidware.New(basicAuthenticator)
+)
+
+func basicHandle(ctx *gem.Context) {
+	ctx.HTML(200, "hello "+basicAuth.Username(ctx))
+}
+
+// digest auth middleware
+var (
+	digestAuthenticator = auth.NewDigestAuthenticator("", func(user, realm string) string {
+		if user == "foo" {
+			// MD5(username:realm:password)
+			return "e0a109b991367f513dfa73bbae05fb07"
+		}
+		return ""
+	})
+
+	digestAuth = authmidware.New(digestAuthenticator)
+)
+
+func digestHandle(ctx *gem.Context) {
+	ctx.HTML(200, "hello "+digestAuth.Username(ctx))
+}
+
+func main() {
+	router := gem.NewRouter()
+
+	// basic auth
+	router.GET("/basic", basicHandle, &gem.HandlerOption{
+		Middlewares: []gem.Middleware{basicAuth},
+	})
+
+	// digest auth
+	router.GET("/digest", digestHandle, &gem.HandlerOption{
+		Middlewares: []gem.Middleware{digestAuth},
+	})
+
+	gem.ListenAndServe(":8080", router.Handler())
+}
+```
--- auth.go
+++ auth.go
@@ -8,7 +8,73 @@ Package authmidware provides HTTP Basic and HTTP Digest authentication for Gem W
 This package depends on go-http-auth, more usages may be obtained on
 https://github.com/abbot/go-http-auth.
 
-Example: see https://github.com/go-gem/examples/tree/master/auth.
+Example
+
+	package main
+
+	import (
+		"github.com/abbot/go-http-auth"
+		"github.com/go-gem/gem"
+		"github.com/go-gem/middleware-auth"
+	)
+
+	var (
+		salt     = []byte("salt")
+		magic    = []byte("$1$")
+		username = "foo"
+		password = []byte("bar")
+	)
+
+	// basic auth middleware
+	var (
+		basicPasswd = string(auth.MD5Crypt(password, salt, magic))
+
+		basicAuthenticator = auth.NewBasicAuthenticator("", func(user, realm string) string {
+			if user == username {
+				return basicPasswd
+			}
+			return ""
+		})
+
+		basicAuth = authmidware.New(basicAuthenticator)
+	)
+
+	func basicHandle(ctx *gem.Context) {
+		ctx.HTML(200, "hello "+basicAuth.Username(ctx))
+	}
+
+	// digest auth middleware
+	var (
+		digestAuthenticator = auth.NewDigestAuthenticator("", func(user, realm string) string {
+			if user == "foo" {
+				// MD5(username:realm:password)
+				return "e0a109b991367f513dfa73bbae05fb07"
+			}
+			return ""
+		})
+
+		digestAuth = authmidware.New(digestAuthenticator)
+	)
+
+	func digestHandle(ctx *gem.Context) {
+		ctx.HTML(200, "hello "+digestAuth.Username(ctx))
+	}
+
+	func main() {
+		router := gem.NewRouter()
+
+		// basic auth
+		router.GET("/basic", basicHandle, &gem.HandlerOption{
+			Middlewares: []gem.Middleware{basicAuth},
+		})
+
+		// digest auth
+		router.GET("/digest", digestHandle, &gem.HandlerOption{
+			Middlewares: []gem.Middleware{digestAuth},
+		})
+
+		gem.ListenAndServe(":8080", router.Handler())
+	}
 */
 package authmidware
 
@@ -19,7 +85,7 @@ import (
 	"github.com/go-gem/gem"
 )
 
-const authContextKey = "auth_name"
+const authContextKey = "gem.auth.name"
 
 // New returns an Auth middleware with the given authenticator.
 func New(authenticator Authenticator) *Auth {
